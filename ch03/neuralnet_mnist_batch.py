d36bc0702f7dc9e8a7491cd684038361dfa50397
Sanghoon <shlee@petaio.com>
2019-07-08 09:18:58 +0900

    Fixed the bugs

--- ch03/neuralnet_mnist_batch.py
+++ ch03/neuralnet_mnist_batch.py
@@ -19,14 +19,14 @@ def init_network():
 
 
 def predict(network, x):
-    w1, w2, w3 = network['W1'], network['W2'], network['W3']
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-    a1 = np.dot(x, w1) + b1
+    a1 = np.dot(x, W1) + b1
     z1 = sigmoid(a1)
-    a2 = np.dot(z1, w2) + b2
+    a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
-    a3 = np.dot(z2, w3) + b3
+    a3 = np.dot(z2, W3) + b3
     y = softmax(a3)
 
     return y
