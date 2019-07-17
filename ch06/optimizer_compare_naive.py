95a8987610865e32ef307145815b6e7969c7e494
Sanghoon <shlee@petaio.com>
2019-07-17 21:11:21 +0900

    Fixed the path issue

--- ch06/optimizer_compare_naive.py
+++ ch06/optimizer_compare_naive.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
