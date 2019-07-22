0c7c918863c3cd95452abd783388ed303abd671a
Sanghoon <shlee@petaio.com>
2019-07-22 18:06:50 +0900

    Added some comments

--- common/functions.py
+++ common/functions.py
@@ -43,17 +43,35 @@ def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
 
+#np.array([0,1,2]) 
+# ndim:1, shape:(3,)
+#np.array([[0,1,2],[3,4,5]])
+# ndim:2, shape:(2,3)
+#np.array([[[0,1,2],[3,4,5]],[[6,7,8],[9,10,11]],[[12,13,14],[15,16,17]]])
+# ndim:3, shape:(4,2,3)
+
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
+        #np.array([0,1,2]) -> np.array([[0,1,2]])
         
     # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    # axis0 : sample index, axis1 : value index
     if t.size == y.size:
         t = t.argmax(axis=1)
              
     batch_size = y.shape[0]
+    # np.arange(100) -> [0, 1, 2, 3, .. , 99]
+    # -np.sum(t * np.log(y))
+    #
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+    # [before optimization...]
+    # sum = 0
+    # for idx in range(0, batch_size):
+    #    sum = -np.sum(t[idx] * np.log(y[idx] + 1e-7))
+    # return sum
 
 
 def softmax_loss(X, t):
